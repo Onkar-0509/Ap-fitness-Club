a170b4923510672d488e248a18d10ab06feacd2a
Onkar-0509 <onkarhadadare05@gmail.com>
2025-01-03 23:30:25 +0530

    Update MemberRoutes.js

--- backend/routes/MemberRoutes.js
+++ backend/routes/MemberRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { memberinfo, info ,deleteMember,updateMember, Profile, checkMembershipExpiration, deleteNotifications } from "../controller/memberController.js";
+import ensureAutheticate from "../Middleware/authMiddleware.js";
 
 const router=express.Router();
 
-router.post("/memberinfo",memberinfo);
-router.get('/info',info);
-router.delete('/memberinfo/:id',deleteMember);
-router.put('/updateMember/:id',updateMember);
-router.post('/profile',Profile);
-router.get('/notifications',checkMembershipExpiration);
-router.delete('/deletenotification/:id',deleteNotifications);
+router.post("/memberinfo",ensureAutheticate,memberinfo);
+router.get('/info',ensureAutheticate,info);
+router.delete('/memberinfo/:id',ensureAutheticate,deleteMember);
+router.put('/updateMember/:id',ensureAutheticate,updateMember);
+router.post('/profile',ensureAutheticate,Profile);
+router.get('/notifications',ensureAutheticate,checkMembershipExpiration);
+router.delete('/deletenotification/:id',ensureAutheticate,deleteNotifications);
 
 
 export default router;
